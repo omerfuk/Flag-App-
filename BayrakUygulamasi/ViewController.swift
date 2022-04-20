3791793da56ece32d5afa06d412ec2fd65b598c5
Omer Faruk Kilicaslan <omerrr_2001@hotmail.com>
2022-04-20 17:47:36 +0300

    Flag App

--- BayrakUygulamasi/ViewController.swift
+++ BayrakUygulamasi/ViewController.swift
@@ -8,13 +8,35 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController{
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    func veritabaniKopyala(){
+        
+        let bundleYolu = Bundle.main.path(forResource: "bayrakquiz", ofType: ".sqlite")
+        
+        let hedefYol = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true).first!
+        
+        let fileManager = FileManager.default
+        
+        let kopyalanacakYer = URL(fileURLWithPath: hedefYol).appendingPathComponent("bayrakquiz.sqlite")
+        
+        if fileManager.fileExists(atPath: kopyalanacakYer.path) {
+            print("Veritabanı zaten var.Kopyalamaya gerek yok")
+        }else{
+            do {
+                
+                try fileManager.copyItem(atPath: bundleYolu!, toPath: kopyalanacakYer.path)
+                
+            }catch{
+                print(error)
+            }
+        }
+    }
 
 }
 
